31a9345f21b7b1158cd1966659823206df1acccc
mhcerri16 <41302987+mhcerri16@users.noreply.github.com>
2025-12-08 23:41:40 -0500

    Add files via upload

---
+++ background.js
@@ -0,0 +1,7 @@
+window.BACKGROUNDS = {
+  "End": "backgrounds/End_Background_dark.webp",
+  "Dirt": "backgrounds/Dirt_Background_dark.webp",
+  "End Stone": "backgrounds/EndStone_Background_dark.webp",
+  "Nether": "backgrounds/Nether_Background_dark.webp",
+  "Portal": "backgrounds/Portal_Background_dark.webp"
+};
